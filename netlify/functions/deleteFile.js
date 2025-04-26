3ff59ac190b477ba46be547537686c4f08faff29
expiredmilk22 <90991765+expiredmilk22@users.noreply.github.com>
2025-04-26 14:46:57 -0700

    Update deleteFile.js

--- netlify/functions/deleteFile.js
+++ netlify/functions/deleteFile.js
@@ -1,6 +1,6 @@
 const cloudinary = require('cloudinary').v2;
 
-// configure cloudinary
+// Configure Cloudinary
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
@@ -8,6 +8,14 @@ cloudinary.config({
 });
 
 exports.handler = async (event) => {
+  // Only allow POST requests
+  if (event.httpMethod !== 'POST') {
+    return {
+      statusCode: 405,
+      body: JSON.stringify({ message: 'Method Not Allowed' }),
+    };
+  }
+
   try {
     const { public_id } = JSON.parse(event.body);
 
